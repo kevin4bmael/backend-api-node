95f5241d49f1645fa965968d408e6aa40e42e334
kevin4bmael <kevin4bmael@gmail.com>
2023-02-10 17:22:31 -0300

    add cursos.json

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const listaCursos = require('./db/cursos.json')
 const app = express()
 const port = 3100
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const msg = [{nome: 'LP2'},{nome: 'PJ3'}]
+  res.json(msg)
 })
+
+app.get('/cursos', (req, res) => {
+    res.json(listaCursos)
+})
+
+app.post('/cursos', (req, res) => {
+  res.json({message: 'POST Cursos JSON!'})
+})
+
+app.put('/cursos', (req, res) => {
+  res.send('Fiz um update no Curso!')
+})
+
+app.all('*', (req, res) =>{
+  res.send('404 Rota não encontrada!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
