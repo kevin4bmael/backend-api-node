17c27f4cadc8b1cc6eb4360453c8181a0146915f
kevin4bmael <kevin4bmael@gmail.com>
2023-02-10 16:00:49 -0300

    add "Hello World" with express

--- src/server.js
+++ src/server.js
@@ -1,27 +1,11 @@
-const http = require('node:http');
-const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3100
 
-const hostname = 'localhost';
-const port = 3100;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server = http.createServer((req, res) => {
-  console.log('URL: ' + req.url)
-  if(req.url === '/'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('src/views/index.html', (error, codigo) => {
-      res.end(codigo);
-    })
-  } else if(req.url === '/cursos'){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('src/views/cursos.html', (error, codigo) => {
-      res.end(codigo);
-    })
-  }
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
